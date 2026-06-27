3594c2382cb2f1d4b03f68b5436c7473b9b9dba9
TotallyNotWh4l3 <atlanticwhale727@gmail.com>
2026-06-28 00:40:47 +0900

    CH1: Toggle Button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-function App() {
+import ToggleFavourite from "./Challanges/ToggleFavourite";
+import ToggleMute from "./Challanges/ToggleMute";
+
+export default function App() {
     return (
         <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
             <h1>Hello Sandbox!</h1>
             <p>Your workspace is completely clean and ready.</p>
+
+            <ToggleFavourite />
+            <ToggleMute />
         </div>
     );
 }
-
-export default App;
